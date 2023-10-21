6a4507c8673ff696f2f22e9e8cec74f5d172e589
caunhach <caunhach@LAPTOP-5U4J17VN.localdomain>
2023-10-21 18:36:07 +0700

    last updated 21/10/2023

--- Days_of_code/Day0/Solution.java
+++ Days_of_code/Day0/Solution.java
@@ -4,10 +4,11 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	public static void main(String[] args){
-		Scanner out = new Scanner(System.in);
-		String result = out.nextLine();
-		System.out.println("Hello, World.");
-		System.out.println(result);
-	}
+
+    public static void main(String[] args) {
+        Scanner out = new Scanner(System.in);
+        String result = out.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(result);
+    }
 }
