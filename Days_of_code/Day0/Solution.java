a522f3e6b98b54a67de52a6923f1c7d94496d4c4
caunhach <caunhach@LAPTOP-5U4J17VN.localdomain>
2023-10-21 18:35:00 +0700

    last updated 21/10/2023

---
+++ Days_of_code/Day0/Solution.java
@@ -0,0 +1,13 @@
+package HackerRank.Days_of_code.Day0;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args){
+		Scanner out = new Scanner(System.in);
+		String result = out.nextLine();
+		System.out.println("Hello, World.");
+		System.out.println(result);
+	}
+}
