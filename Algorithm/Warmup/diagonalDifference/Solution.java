6aee7a5e5359d5343df10cf67948f3ff54a2a78b
caunhach <caunhach@LAPTOP-5U4J17VN.localdomain>
2023-10-22 15:50:45 +0700

    last updated 22/10/2023

--- Algorithm/Warmup/diagonalDifference/Solution.java
+++ Algorithm/Warmup/diagonalDifference/Solution.java
@@ -13,13 +13,13 @@ import java.util.stream.Stream;
 
 class Result {
     public static int diagonalDifference(List<List<Integer>> arr) {
-		int sum_lr = 0;
-		int sum_rl = 0;
-		for (int i =0; i < arr.size(); i++){
-			sum_lr += arr.get(i).get(i);
-			sum_rl += arr.get(arr.size() - 1 -i).get(arr.size() - 1 - i);
-		}
-		return Math.abs(sum_lr - sum_rl);
+        int sum_lr = 0;
+        int sum_rl = 0;
+        for (int i =0; i < arr.size(); i++){
+            sum_lr += arr.get(i).get(i);
+            sum_rl += arr.get(i).get(arr.size() - 1 - i);
+        }
+        return Math.abs(sum_lr - sum_rl);
     }
 }
 
